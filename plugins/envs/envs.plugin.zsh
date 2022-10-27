240ff1195a17c280ea008f9b4fcb48bf98cf4a45
Jorge Saud <jsaud@modyo.com>
2022-10-27 17:21:24 -0300

    chore: support

--- plugins/envs/envs.plugin.zsh
+++ plugins/envs/envs.plugin.zsh
@@ -1,6 +1,2 @@
-# Add your own custom plugins in the custom/plugins directory. Plugins placed
-# here will override ones with the same name in the main plugins directory.
-export MODYO_MYSQL_DATABASE=modyo
-export MODYO_MYSQL_USERNAME=giorgiosaud
-export MODYO_MYSQL_PASSWORD=123123
-export MODYO_MYSQL_HOST=localhost
+# Add your own secrets here example
+#export MODYO_MYSQL_DATABASE=databaseName
